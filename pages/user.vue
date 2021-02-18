988879e3aa49911c8e26707dc80aef3a7f2ce659
linpeijian <linpeijian@qq.com>
2021-02-18 11:15:18 +0800

    feat: 测试嵌套路由

---
+++ pages/user.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    我才是User 测试嵌套路由
+    <nuxt-child></nuxt-child>
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+@Component
+export default class extends Vue {
+  mounted() {
+    // todo
+  }
+}
+</script>
